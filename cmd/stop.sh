914dac38b1e45b6efd900d6cabd78d42475d12ca
Chihyun Song <ian0371@gmail.com>
2022-09-30 18:54:07 +0900

    start/stop gets #vals from output dir

--- cmd/stop.sh
+++ cmd/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 stop() {
-    vals=$(ls -1 $ROOTDIR/homi-output/keys/nodekey* | wc -l)
+    vals=$(ls -1d $ROOTDIR/output/cn* | wc -l)
 
     for i in $(seq 1 $vals); do
         pushd "$ROOTDIR/output/cn$i"
