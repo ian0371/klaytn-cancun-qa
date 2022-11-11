a4eb89019d5cf9b953df38803341737e08691142
Chihyun Song <ian0371@gmail.com>
2022-11-11 19:21:51 +0900

    add init, upgrade

---
+++ cmd/upgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+upgrade() {
+    vals=$(ls -1d $ROOTDIR/output/cn* | wc -l)
+
+    for i in $(seq 1 $vals); do
+        pushd "$ROOTDIR/output/cn$i"
+        cp `which kcn` "$ROOTDIR/kcnd" bin/
+        popd
+    done
+}
